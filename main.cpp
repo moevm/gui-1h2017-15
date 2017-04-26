a871122a5aab83c3b3318d0b2498b2380b069a42
JuliaPol <287jpi@gmail.com>
2017-04-26 17:33:24 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,8 +6,8 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
-//    LoginWindow w;
-    MailBox w;
+    LoginWindow w;
+    //MailBox w;
     w.show();
 
     return a.exec();
